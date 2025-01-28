e10bcba9af0e0bd309d5beda7fdcebc83fb05f29
saipranay526 <vardellisaipranay@gmail.com>
2025-01-28 23:04:33 +0530

    Add files via upload

---
+++ Rightshift.java
@@ -0,0 +1,8 @@
+
+public class Rightshift {
+    public static void main(String[] args){
+        int num = 10; // Binary representation: 1010
+        int shiftedNum = num >> 1; // Shift right by 1 bit
+        System.out.println("Original Number: " + num);
+        System.out.println("Shifted Number: " + shiftedNum);
+    }
